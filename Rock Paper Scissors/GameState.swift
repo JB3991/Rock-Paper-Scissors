69877ddd83d2d579fa781540fd818688c70fe7f4
Jon Burnett <74561172+JB3991@users.noreply.github.com>
2021-01-03 13:43:48 +0000

    First Commit
    Uploadig Rock, Paper, Scissors game

---
+++ Rock Paper Scissors/GameState.swift
@@ -0,0 +1,13 @@
+//
+//  GameState.swift
+//  Rock Paper Scissors
+//
+//  Created by Jonathan Burnett on 03/01/2021.
+//
+
+import Foundation
+
+enum GameState {
+    case start, win, lose, draw
+}
+
